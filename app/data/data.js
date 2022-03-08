6fadffe8e29ae22f504f1f2ad1c414ee844fa434
Princeton Dychinco <prdychinco@gmail.com>
2022-03-08 13:15:36 -0800

    added json and rendering json

---
+++ app/data/data.js
@@ -0,0 +1,73 @@
+[
+      {
+        "name":"Nvidia Geforce RTX 3090",
+        "current price":"2,049",
+        "msrp":"1,499",
+        "stock":"unavailable",
+        "memory":"24GB"
+      },
+      {
+        "name":"Nvidia Geforce RTX 3080",
+        "current price":"1,029",
+        "msrp":"699",
+        "stock":"unavailable",
+        "memory":"10GB"
+      },
+      {
+        "name":"Nvidia Geforce RTX 3070",
+        "current price":"799",
+        "msrp":"499",
+        "stock":"unavailable",
+        "memory":"8GB"
+      },
+      {
+        "name":"Nvidia Geforce RTX 3060",
+        "current price":"629",
+        "msrp":"329",
+        "stock":"unavailable",
+        "memory":"8GB"
+      },
+      {
+        "name":"Nvidia Geforce RTX 3050",
+        "current price":"499",
+        "msrp":"249",
+        "stock":"unavailable",
+        "memory":"8GB"
+      },
+      {
+        "name":"AMD Radeon RX 6900 XT",
+        "current price":"1,270",
+        "msrp":"999",
+        "stock":"unavailable",
+        "memory":"16GB"
+      },
+      {
+        "name":"AMD Radeon RX 6800 XT",
+        "current price":"825",
+        "msrp":"649",
+        "stock":"unavailable",
+        "memory":"16GB"
+      },
+      {
+        "name":"AMD Radeon RX 6700 XT",
+        "current price":"610",
+        "msrp":"479",
+        "stock":"unavailable",
+        "memory":"12GB"
+      },
+      {
+        "name":"AMD Radeon RX 6600 XT",
+        "current price":"550",
+        "msrp":"379",
+        "stock":"unavailable",
+        "memory":"8GB"
+      },
+      {
+        "name":"AMD Radeon RX 6600",
+        "current price":"520",
+        "msrp":"329",
+        "stock":"unavailable",
+        "memory":"8GB"
+
+  }
+]
--- public/js/client.js
+++ public/js/client.js
@@ -1,4 +1,38 @@
-function loadSkeleton(){
-  console.log($('#tablePlaceholder').load('../../app/data/data.html'));
+console.log("Client script loaded.");
+
+// a function declaration inside of a callback ... which takes a callback function :O
+function ajaxGET(url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            //console.log('responseText:' + xhr.responseText);
+            callback(this.responseText);
+
+        } else {
+            console.log(this.status);
+        }
+    }
+    xhr.open("GET", url);
+    xhr.send();
 }
-loadSkeleton();  //invoke the function
+
+ajaxGET("/data/data.html", function (data) {
+  document.getElementById("tablePlaceholder").innerHTML = data;
+});
+
+ajaxGET("/data/data.js", function (data) {
+  console.log("Before parsing", data);
+  // this call is JSON so we have to parse it:
+  let parsedData = JSON.parse(data);
+  console.log("Before parsing", parsedData);
+  let str = "<ol>"
+  for (let i = 0; i < parsedData.length; i++) {
+      str += "<li>" + parsedData[i]["name"] + "</li>";
+      str += "<li>" + parsedData[i]["current price"] + "</li>";
+      str += "<li>" + parsedData[i]["msrp"] + "</li>";
+      str += "<li>" + parsedData[i]["stock"] + "</li>";
+      str += "<li>" + parsedData[i]["memory"] + "</li>";
+  }
+  str += "</ol>";
+  document.getElementById("jsonPlaceholder").innerHTML = str;
+});
